c0b2388100d026b29ac8d63bd6f2c6addfca7b8f
gaoyang <gy@gmail.com>
2021-03-23 16:10:55 +0800

    add 2 security audits

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e17616da4a6d99a7f5a55bd81319817.js"
+  "/precache-manifest.4c505cc943017976fbe7e5952437de16.js"
 );
 
 self.addEventListener('message', (event) => {
